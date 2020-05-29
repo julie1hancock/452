c735d5201caccddfcbdf88cac81c13c483a755d5
Julie Hancock <julieh1023@gmail.com>
2020-05-29 16:20:59 -0600

    finished xml for first

--- app/src/main/java/view/MainActivity.kt
+++ app/src/main/java/view/MainActivity.kt
@@ -10,48 +10,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        setSupportActionBar(toolbar)
-//        toolbar.setNavigationOnClickListener { onBackPressed() }
-        //vm = new
+        toolbar.setNavigationOnClickListener { onBackPressed() }
         setupListeners()
     }
 
     private fun setupListeners() {
         setupImportantToggle()
-
-
-
     }
 
-    var importantSelected = 0
+    var importantSelected = 1
     private fun setupImportantToggle() {
-        mostImportantOption1.setOnCheckedChangeListener{ button, isChecked ->
-            if(isChecked && importantSelected != 1) {
-                importantSelected = 1
-                mostImportantOption1.setBackgroundColor(resources.getColor(R.color.selected))
-                mostImportantOption2.setBackgroundColor(resources.getColor(R.color.not_selected))
-                mostImportantOption3.setBackgroundColor(resources.getColor(R.color.not_selected))
-            }
-        }
-        mostImportantOption2.setOnCheckedChangeListener{ button, isChecked ->
-            if(isChecked && importantSelected != 2) {
-                importantSelected = 2
-                mostImportantOption2.setBackgroundColor(resources.getColor(R.color.selected))
-                mostImportantOption1.setBackgroundColor(resources.getColor(R.color.not_selected))
-                mostImportantOption3.setBackgroundColor(resources.getColor(R.color.not_selected))
-            }
-        }
-        mostImportantOption3.setOnCheckedChangeListener{ button, isChecked ->
-            if(isChecked && importantSelected != 3) {
-                importantSelected = 3
-                mostImportantOption3.setBackgroundColor(resources.getColor(R.color.selected))
-                mostImportantOption2.setBackgroundColor(resources.getColor(R.color.not_selected))
-                mostImportantOption1.setBackgroundColor(resources.getColor(R.color.not_selected))
-            }
-        }
-        mostImportantOption2.isChecked = false //none
-        mostImportantOption3.isChecked = false //none
-        mostImportantOption1.isChecked = true //first
+//        mostImportantOption1.setOnCheckedChangeListener{ button, isChecked ->
+//            importantSelected = 1
+//            mostImportantOption1.setBackgroundColor(resources.getColor(R.color.selected))
+//            mostImportantOption2.setBackgroundColor(resources.getColor(R.color.not_selected))
+//            mostImportantOption3.setBackgroundColor(resources.getColor(R.color.not_selected))
+//        }
+//        mostImportantOption2.setOnCheckedChangeListener{ button, isChecked ->
+//            importantSelected = 2
+//            mostImportantOption1.setBackgroundColor(resources.getColor(R.color.not_selected))
+//            mostImportantOption2.setBackgroundColor(resources.getColor(R.color.selected))
+//            mostImportantOption3.setBackgroundColor(resources.getColor(R.color.not_selected))
+//        }
+//        mostImportantOption3.setOnCheckedChangeListener{ button, isChecked ->
+//            importantSelected = 3
+//            mostImportantOption1.setBackgroundColor(resources.getColor(R.color.not_selected))
+//            mostImportantOption2.setBackgroundColor(resources.getColor(R.color.not_selected))
+//            mostImportantOption3.setBackgroundColor(resources.getColor(R.color.selected))
+//        }
+//        mostImportantOption1.isChecked = true //first
 
     }
 
