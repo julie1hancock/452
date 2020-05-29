1581bf4958064b94f7ecd9fa0edc7a6879ce5c46
Julie Hancock <julieh1023@gmail.com>
2020-05-29 16:37:31 -0600

    added new module

--- app/src/main/java/view/MainActivity.kt
+++ app/src/main/java/view/MainActivity.kt
@@ -1,11 +1,21 @@
 package view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import hancock.julie.temp452project.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var importantSelected = 1
+    var citySelected = 1
+    var monthlyRent = 0.0
+    var hasWasherDryer = true
+    var numBedrooms = 0
+    var numBathrooms = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,31 +26,66 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupListeners() {
         setupImportantToggle()
+        setupCityToggle()
+        monthlyRentValue.afterTextChanged { monthlyRent = it.toDouble() }
+        numBedroomsValue.afterTextChanged { numBedrooms = it.toInt() }
+        numBathroomsValue.afterTextChanged { numBathrooms = it.toDouble() }
+        washerDryerValue.setOnCheckedChangeListener{ _, isChecked ->
+            hasWasherDryer = isChecked
+        }
+
     }
 
-    var importantSelected = 1
     private fun setupImportantToggle() {
-//        mostImportantOption1.setOnCheckedChangeListener{ button, isChecked ->
-//            importantSelected = 1
-//            mostImportantOption1.setBackgroundColor(resources.getColor(R.color.selected))
-//            mostImportantOption2.setBackgroundColor(resources.getColor(R.color.not_selected))
-//            mostImportantOption3.setBackgroundColor(resources.getColor(R.color.not_selected))
-//        }
-//        mostImportantOption2.setOnCheckedChangeListener{ button, isChecked ->
-//            importantSelected = 2
-//            mostImportantOption1.setBackgroundColor(resources.getColor(R.color.not_selected))
-//            mostImportantOption2.setBackgroundColor(resources.getColor(R.color.selected))
-//            mostImportantOption3.setBackgroundColor(resources.getColor(R.color.not_selected))
-//        }
-//        mostImportantOption3.setOnCheckedChangeListener{ button, isChecked ->
-//            importantSelected = 3
-//            mostImportantOption1.setBackgroundColor(resources.getColor(R.color.not_selected))
-//            mostImportantOption2.setBackgroundColor(resources.getColor(R.color.not_selected))
-//            mostImportantOption3.setBackgroundColor(resources.getColor(R.color.selected))
-//        }
-//        mostImportantOption1.isChecked = true //first
+        mostImportantOption1.setOnCheckedChangeListener{ _, _ ->
+            importantSelected = 1
+            mostImportantOption1.setBackgroundColor(resources.getColor(R.color.selected))
+            mostImportantOption2.setBackgroundColor(resources.getColor(R.color.not_selected))
+            mostImportantOption3.setBackgroundColor(resources.getColor(R.color.not_selected))
+        }
+        mostImportantOption2.setOnCheckedChangeListener{ _, _ ->
+            importantSelected = 2
+            mostImportantOption1.setBackgroundColor(resources.getColor(R.color.not_selected))
+            mostImportantOption2.setBackgroundColor(resources.getColor(R.color.selected))
+            mostImportantOption3.setBackgroundColor(resources.getColor(R.color.not_selected))
+        }
+        mostImportantOption3.setOnCheckedChangeListener{ _, _ ->
+            importantSelected = 3
+            mostImportantOption1.setBackgroundColor(resources.getColor(R.color.not_selected))
+            mostImportantOption2.setBackgroundColor(resources.getColor(R.color.not_selected))
+            mostImportantOption3.setBackgroundColor(resources.getColor(R.color.selected))
+        }
+        mostImportantOption1.isChecked = true //first
 
     }
 
+    private fun setupCityToggle() {
+        city1.setOnCheckedChangeListener{ _, _ ->
+            citySelected = 1
+            city1.setBackgroundColor(resources.getColor(R.color.selected))
+            city2.setBackgroundColor(resources.getColor(R.color.not_selected))
+        }
+        city2.setOnCheckedChangeListener{ _, _ ->
+            citySelected = 2
+            city1.setBackgroundColor(resources.getColor(R.color.not_selected))
+            city2.setBackgroundColor(resources.getColor(R.color.selected))
+        }
+        city1.isChecked = true
+    }
 
+
+}
+
+fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun afterTextChanged(editable: Editable?) {
+            afterTextChanged.invoke(editable.toString())
+        }
+    })
 }
